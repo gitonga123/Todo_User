e593647e4616601935c18181fe712509491dd41a
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-02-23 10:08:14 +0300

    Model inheritance and extenstion

--- models/models.py
+++ models/models.py
@@ -4,11 +4,20 @@ from openerp import models, fields, api
 
 
 
-class TodoUser(models.Model):
+class TodoTask(models.Model):
 	_inherit ='todo.two'
 	user_id = fields.Many2one('res.user', 'Responsible')
 	date_deadline = fields.Date('Deadline')
 	name = fields.Char(help="What needs to be done?")
+	@api.multi
+	def do_toggle_done(self):
+		for task in self:
+			if task.user_id != self.env.user:
+				raise ValidationError(
+					'Only the responsible can do this!')
+
+		return super(TodoTask, self).do_toggle_done()
+
 
 	@api.multi
 	def do_clear_done(self):
@@ -17,3 +26,4 @@ class TodoUser(models.Model):
 					('user_id','=',False)]
 		dones = self.search(domain)
 		dones.write({'active': False})
+		return True
