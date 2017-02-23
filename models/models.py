89966214d656d0f59b5a47923cda7540f583531a
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-02-23 09:59:33 +0300

    Model inheritance and extenstion

--- models/models.py
+++ models/models.py
@@ -9,3 +9,11 @@ class TodoUser(models.Model):
 	user_id = fields.Many2one('res.user', 'Responsible')
 	date_deadline = fields.Date('Deadline')
 	name = fields.Char(help="What needs to be done?")
+
+	@api.multi
+	def do_clear_done(self):
+		domain = [('is_done', '=', True),
+					'|',('user_id', '=',self.env.uid),
+					('user_id','=',False)]
+		dones = self.search(domain)
+		dones.write({'active': False})
